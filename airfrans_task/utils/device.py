a27ca6c0e26b38e4cec7ff367b415754f30e22a4
Giovanni Catalani <62262662+giovannicatalani@users.noreply.github.com>
2025-05-15 16:54:53 +0200

    Delete airfrans_task/utils/device.py

--- airfrans_task/utils/device.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# DEVICE = torch.device(2 if torch.cuda.is_available() else 'cpu')
-# DEVICE = torch.device('cpu')
